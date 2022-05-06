a0f71c716716a2a6ab82550e9ad41b971b8a15de
herlnd <hernanlandoni@gmail.com>
2022-05-06 20:50:49 -0300

    updated code and style

--- odin-projects/calculator/calculator.js
+++ odin-projects/calculator/calculator.js
@@ -1,147 +1,173 @@
-// Keyboard elements
-const numbers = document.querySelectorAll('.number');
+const topDisplay = document.getElementById('top-display');
+const mainDisplay = document.getElementById('main-display');
+const clearButtons = document.querySelectorAll('.clear');
 const operators = document.querySelectorAll('.operator');
-const decimal = document.getElementById('decimal');
+const numbers = document.querySelectorAll('.number');
 const plusMinus = document.getElementById('plus-minus');
-const clear = document.getElementById('clear-all');
-const clearEntry = document.getElementById('clear-entry');
-const deleteButton = document.getElementById('delete');
-const equal = document.getElementById('equal');
+const decimal = document.getElementById('decimal');
 
-// Operators array
-let operatorsArr = [];
-operators.forEach(operator => operatorsArr.push(operator.textContent));
+clearButtons.forEach(button => button.addEventListener('click', clearX));
+numbers.forEach(num => num.addEventListener('click', addNumber));
+operators.forEach(op => op.addEventListener('click', addOperator));
+plusMinus.addEventListener('click', changeSign);
+decimal.addEventListener('click', addDecimal);
 
-// Display elements
-let displayTop = document.getElementById('operation');
-let displayMain = document.getElementById('result');
-
-// Event listeners
-numbers.forEach(number => number.addEventListener('click', showOnDisplay));
-operators.forEach(operator => operator.addEventListener('click', showOnDisplay));
-decimal.addEventListener('click', showOnDisplay);
-clear.addEventListener('click', clearAll);
-clearEntry.addEventListener('click', clearTextContent);
-deleteButton.addEventListener('click', deleteChar);
-plusMinus.addEventListener('click', showOnDisplay);
-equal.addEventListener('click', showOnDisplay);
-
-// Check for operators
-function containsOperator(str) {
-    if (operatorsArr.some(operator => str.indexOf(operator) !== -1)) {
-        return true;
-    } else return false;
+// Adds a decimal to the display/input
+function addDecimal() {
+    let disp = getMainDispContent();
+    if (disp.indexOf(',') !== -1) {
+        showOnMainDisp(disp);
+    } else {
+        showOnMainDisp(`${disp},`);
+    };
 };
 
-function containsDecimal(str) {
-    if (str.indexOf(',') !== -1) {
-        return true;
-    } else return false;
+// Changes the sign of the display/input.
+function changeSign() {
+    let disp = getMainDispContent();
+    if (disp === '0') {
+        showOnMainDisp(disp);
+    } else if (disp.startsWith('-')) {
+        showOnMainDisp(disp.slice(1)); 
+    } else {
+        showOnMainDisp(`-${disp}`);
+    };
 };
 
-// Equation elements
+// Clear functions.
+function clearX(e) {
+    switch (e.target.id) {
+        case 'delete':
+            let disp = getMainDispContent();
+            if (disp.length === 1) {
+                showOnMainDisp('0');
+            } else {
+                showOnMainDisp(disp.slice(0, disp.length - 1)); 
+            };
+        break;
+
+        case 'clear-entry':
+            showOnMainDisp('0');
+        break;
+
+        case 'clear-all':
+            showOnMainDisp('0');
+            showOnTopDisp('');
+            console.clear();
+        break;
+    };
+};
+
+// Sets display to default values.
+const setDisplay = (e) => {
+    if (mainDisplay.textContent === '0' && (e.target.className !== 'operator' &&
+    e.target.id !== 'decimal')) {
+    mainDisplay.textContent = '';
+    };
+};
+
+// Adds numbers to the main display
+function addNumber(e) {
+    setDisplay(e);
+    let disp = getMainDispContent();
+    if (disp.length < 13) {
+        mainDisplay.textContent += e.target.textContent;
+    };
+};
+
+function addNumber2(e) {
+    setDisplay(e);
+    let disp = getMainDispContent();
+    if (disp.length < 13) {
+        mainDisplay.textContent += 'tato';
+    };
+};
+
+// Equation terms
 let leftHandSide;
 let rightHandSide;
-let operatorSymbol;
-let operationResult;
+let result;
 
-function deleteChar() {
-    if (displayMain.textContent.length === 1) {
-        displayMain.textContent = '0';
+// Operators array
+let auxOpArr = [];
+operators.forEach(op => auxOpArr.push(op.textContent));
+let opArr = auxOpArr.filter(operator => operator.length === 1 && operator !== '%');
+
+// Check for operators
+const containsOp = (str) => opArr.some(op => str.indexOf(op) !== -1 ? true : false);
+
+// Get and show content on display.
+const getTopDispContent = () => topDisplay.textContent;
+const getMainDispContent = () => mainDisplay.textContent;
+const showOnMainDisp = (str) => mainDisplay.textContent = str;
+const showOnTopDisp = (str) => topDisplay.textContent = str;
+
+// Adds operator to the top display and saves the previous term in the left-hand side of the equation.
+function addOperator(e) {
+    let disp = getMainDispContent();
+    if (disp === 'overflow' || disp === 'NaN') {
+        leftHandSide = 0;
+    } else  {
+        leftHandSide = parseFloat(disp.replace('.', '').replace(',', '.'));
+    };
+
+    if (containsOp(e.target.textContent)) {
+        showOnTopDisp(`${leftHandSide} ${e.target.textContent}`);
     } else {
-        let dispStr = displayMain.textContent;
-        displayMain.textContent = dispStr.slice(0, dispStr.length - 1);
+        let calcObj = calculateOthers(leftHandSide, e.target.id);
+        showOnMainDisp(`${calcObj.result}`);
+        showOnTopDisp(`${calcObj.expression}`);
     };
 };
 
-function showOnDisplay(e) {
-    if (displayMain.textContent === '0' && (e.target.className !== 'operator' &&
-        e.target.id !== 'decimal')) {
-        displayMain.textContent = '';
-    };
-
-    switch (true) {
-        case (e.target.className === 'number'):
-            if (!containsOperator(displayTop.textContent)) {
-                displayMain.textContent += e.target.textContent;
-            };
-            break;
-
-        case (e.target.id === 'decimal'):
-            if (!containsDecimal(displayMain.textContent)) {
-                displayMain.textContent += e.target.textContent;
-            };
-            break;
-
-        case (e.target.id === 'plus-minus'):
-            if (displayMain.textContent.startsWith('-')) {
-                displayMain.textContent = displayMain.textContent.slice(1);
-            } else {
-                displayMain.textContent = `-${displayMain.textContent}`;
-            };
-            break;
-
-        case (e.target.id === 'equal'):
-            console.log('equal');
-            break;
-
-        case (e.target.className === 'operator'):
-            console.log(e.target.textContent);
-            switch (e.target.textContent) {
-                case '%':
-                    leftHandSide = parseFloat(displayMain.textContent.replace(',', '.')); 
-                    operationResult = leftHandSide / 100;
-                    displayMain.textContent = operationResult.toString().replace('.', ',');
-                    leftHandSide = operationResult;
-                    break;
-
-                case '1/x':
-                    if (parseFloat(displayMain.textContent.replace(',', '.')) !== 0) {
-                        leftHandSide = parseFloat(displayMain.textContent.replace(',', '.')); 
-                        operationResult = 1 / leftHandSide;
-                        displayMain.textContent = operationResult.toString().replace('.', ',');
-                        leftHandSide = operationResult;
-                    } else {
-                        alert('nope')
-                    }
-                    break;
-
-                case 'x²':
-                    leftHandSide = parseFloat(displayMain.textContent.replace(',', '.'));
-                    operationResult = leftHandSide ** 2;
-                    displayMain.textContent = operationResult.toString().replace('.', ',');
-                    leftHandSide = operationResult; 
-                    break;
-
-                case '2√x':
-                    if (parseFloat(displayMain.textContent.replace(',', '.')) > 0) {
-                        leftHandSide = parseFloat(displayMain.textContent.replace(',', '.'));
-                        operationResult = leftHandSide ** (1/2);
-                        displayMain.textContent = operationResult.toString().replace('.', ',');
-                        leftHandSide = operationResult; 
-                    } else {
-                        alert('nope');
-                    }
-                    break;
-
-                default:
-                    displayMain.textContent += e.target.textContent;
-                    displayTop.textContent = displayMain.textContent;
-                    displayMain.textContent = displayMain.textContent.slice(0, 
-                    displayMain.textContent.length - e.target.textContent.length);
-                    break;
-            };
+// Calculates %, 1/x, pow and sqrt.
+function calculateOthers(leftTerm, operator) {
+    switch (operator) {
+        case 'percentage':
+            result = formatNumber((leftTerm / 100));
+            expression = `${leftTerm}%`;
+            return {result, expression};
+        case 'fraction':
+            leftTerm === 0 ? alert('nope') : result = formatNumber(1 / leftTerm);
+            expression = `1/${leftTerm}`;
+            return {result, expression};
+        case 'pow':
+            result = formatNumber(leftTerm ** 2);
+            expression = `${leftTerm}²`;
+            return {result, expression};
+        case 'sqrt':
+            leftTerm < 0 ? alert('nope') : result = formatNumber((leftTerm ** (1/2)));
+            expression = `2√${leftTerm}`;
+            return {result, expression};
     };
 };
 
-// Clears display
-function clearAll() {
-    displayTop.textContent = '';
-    displayMain.textContent = 0;
-    console.clear();
+// Formats numbers to local language representation.
+const formatNumber = (num) => {
+    let expIndex = num.toString().indexOf('e+');
+    if (expIndex !== -1) {
+        formattedNumber = 'overflow';
+    } else if (num.toString().length > 10) {
+        let auxNum = num.toLocaleString('es-AR', {maximumFractionDigits: 2, notation: 'scientific'});
+        let eIndex = auxNum.indexOf('E0');
+        if (eIndex !== -1) {
+            formattedNumber = auxNum.slice(0, eIndex);
+        } else {
+            formattedNumber = auxNum;
+        };
+    } else {
+        formattedNumber = num.toLocaleString('es-AR', {maximumFractionDigits: 2});
+    };
+
+    return formattedNumber;
 };
 
-// Clears last entry
-function clearTextContent() {
-    displayMain.textContent = 0;
+// Handles engineering notations.
+function handleNotation(num) {
+    let notationIndex = num.toString().indexOf('E');
+    if (notationIndex !== -1) {
+        return num.toString().replace('E', 'exp');
+    } else {
+        return num;
+    };
 };
