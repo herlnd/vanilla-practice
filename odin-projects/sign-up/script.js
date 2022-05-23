f7603e06a5baa48a597fcd9a0fb53980a3141b3b
herlnd <hernanlandoni@gmail.com>
2022-05-23 20:28:16 -0300

    uploaded new practice project

---
+++ odin-projects/sign-up/script.js
@@ -0,0 +1,11 @@
+const password = document.getElementById('password')
+const confirmPassword = document.getElementById('confirm-password');
+
+const validatePassword = () => {
+    password.value != confirmPassword.value ?
+    confirmPassword.setCustomValidity('Passwords don\'t match') :
+    confirmPassword.setCustomValidity('');
+};
+
+password.onchange = validatePassword;
+confirmPassword.onkeyup = validatePassword;
